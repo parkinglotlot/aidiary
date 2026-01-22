9d57063047c2bccea237a94e352d18b066524f74
Park Youjin <ujin2597.u@gmail.com>
2026-01-22 23:25:41 +0900

    diary 페이징 테스트중

--- diary/src/main/java/com/aidiary/diary/jpa/Diary.java
+++ diary/src/main/java/com/aidiary/diary/jpa/Diary.java
@@ -9,14 +9,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Diary {
 
   @Id
