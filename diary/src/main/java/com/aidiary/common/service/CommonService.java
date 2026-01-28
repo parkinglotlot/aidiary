ef5eee0f5e156c4228c86a2b60f1b0b0fb21e9c9
Park Youjin <ujin2597.u@gmail.com>
2026-01-29 00:11:24 +0900

    클라이언트 류저 전송 삭제

--- diary/src/main/java/com/aidiary/common/service/CommonService.java
+++ diary/src/main/java/com/aidiary/common/service/CommonService.java
@@ -19,6 +19,7 @@ public class CommonService {
   private final DiaryMapper diaryMapper;
 
     // 로그인 유저 존재 확인 및 현재 로그인 유저가 해당 아이디의 유저인지 확인 (Validation)
+    // 이건 다음에  필요할 때 사용
     public void validateUser(String sessionLoginId,String loginId){
 
     // 로그인 유저 존재 확인
@@ -39,6 +40,26 @@ public class CommonService {
     //2. 로그인 유저
     boolean isLoginUser = sessionLoginId.equals(sessionLoginId);
     if(!isLoginUser) throw new CustomException(customResponse,httpStatusBadRequest);
+  }
+
+  public User validateUserEmpty(String sessionLoginId){
+
+    // 로그인 유저 존재 확인
+    User user = userRepository.getUserByLoginId(sessionLoginId);
+
+    //httpstatus 세팅
+    HttpStatus httpStatusBadRequest = HttpStatus.BAD_REQUEST;
+    HttpStatus httpStatusUnauthorized = HttpStatus.UNAUTHORIZED;
+
+    //CustomResponseEntity 반환
+    CustomResponseEntity customResponse = new CustomResponseEntity();
+
+    if(user == null){
+      throw new CustomException(customResponse,httpStatusBadRequest);
+    }
+
+    return user;
+
   }
 
 
